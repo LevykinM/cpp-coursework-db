d333210425ebc2a4338f9301fafe8d2b6a534bb7
LevykinM <michailinlevyk@gmail.com>
2026-05-25 23:13:17 +0500

    Delete 02-variables directory

--- 02-variables/task1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-    double pi = 3.14159;
-    double radius = 5.0;
-    double area = pi * (radius * radius);
-    std::cout << "Circle area for r = " << radius << " : " << area << "\n";
-    return 0;
-}
